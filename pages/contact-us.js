9e49714bc031b1f8d17d814c1ba00efb572b1545
snehal <mahabaresnehal@gmail.com>
2023-04-24 13:27:11 +0530

    contact us page

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -4,7 +4,7 @@ import { contactUsBreadCrums } from "../utils/data/breadcrumbs";
 const ContactUs = () => {
   return (
     <>
-      <Breadcrumbs data={contactUsBreadCrums} />
+      {/* <Breadcrumbs data={contactUsBreadCrums} /> */}
       <h1 style={{ textAlign: "center", padding: "20px" }}>
         Coming soon Contact us
       </h1>
